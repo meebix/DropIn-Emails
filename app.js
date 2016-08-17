6e82ee868a345a93e9b4cc3cd74a868223bb9d2b
Mike Donahue <mtdonahue1@gmail.com>
2016-08-16 23:12:57 -0700

    Update template, add helper

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ var timezone = require('moment-timezone');
 var Handlebars = require('handlebars');
 var groupBy = require('handlebars-group-by');
 Handlebars.registerHelper(groupBy(Handlebars));
+Handlebars.registerHelper('grouped_each', function(every, context, options) {
+  var out = "", subcontext = [], i;
+  if (context && context.length > 0) {
+    for (i = 0; i < context.length; i++) {
+      if (i > 0 && i % every === 0) {
+        out += options.fn(subcontext);
+        subcontext = [];
+      }
+      subcontext.push(context[i]);
+    }
+    out += options.fn(subcontext);
+  }
+  return out;
+});
 
 var templateDir = path.resolve(__dirname, 'templates', 'july-2016', 'active-rewards');
 var template = new EmailTemplate(templateDir);
