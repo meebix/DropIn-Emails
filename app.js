ed735a1ca910c210bf346c40eb2e7b3ede21839b
Mike Donahue <mtdonahue1@gmail.com>
2016-10-29 15:18:42 -0700

    Enabling email sending

--- app.js
+++ app.js
@@ -191,12 +191,11 @@ var getAddress = function(pageCount, url) {
                 };
 
                 if (userData.rewards.length) {
-                  console.log(userData.email + ', ' + 'Successfully sent the email to this user.');
-                  // return mailgun.messages().send(emailData).then(function(result) {
-                  //   console.log(userData.email + ', ' + 'Successfully sent the email to this user.');
-                  // }, function(err) {
-                  //   console.log(userData.email + ', ' + 'There was an error sending the email to this user: ' + {error: error});
-                  // });
+                  return mailgun.messages().send(emailData).then(function(result) {
+                    console.log(userData.email + ', ' + 'Successfully sent the email to this user.');
+                  }, function(err) {
+                    console.log(userData.email + ', ' + 'There was an error sending the email to this user: ' + {error: error});
+                  });
                 } else {
                   console.log(userData.email + ', ' + 'This user was skipped because they have no rewards meeting the criteria to send the email.');
                 }
